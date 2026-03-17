61b86ded772bf7d25f92f025a4b3538b1504a791
Janine Mouvet <jmouvet@ikado.de>
2026-03-17 10:05:48 +0100

    # TODO:

--- main.py
+++ main.py
@@ -14,3 +14,15 @@ if __name__ == "__main__":
     app = ImageViewer(root, folder)
     root.mainloop()
 
+    # ----------------------------------------------------------------------------------
+    # TODO:
+    # 1) Add Program Root for all Projects. Must auto ask if null
+    # 2) Add Column (hideable) with project sets info on left under menu
+    #       - add view button next to menu. All columns hideable and retrievable
+    #       - clean view with info bar only, shortcuts work for clean annot. option
+    #       - double click set name to switch to it (autosave changes on change)
+    # 3) Add class colours to class column
+    # 4) Add Users to project
+    # 5) Zoom function for high-res/small-object annotation (small buttons + shortcut)
+    # 6) Think about central save options
+    # -----------------------------------------------------------------------------------

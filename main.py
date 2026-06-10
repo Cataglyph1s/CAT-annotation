477f826ea9d1ea80eba49b053914b2fd887d789f
Janine Mouvet <jmouvet@ikado.de>
2026-06-10 09:57:04 +0200

    v0.4.1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from tkinter import filedialog, messagebox  # used in _ensure_program_root
 from image_viewer_controller import ImageViewerController
 from app_config import AppConfig
 
-VERSION = "v0.4.0"
+VERSION = "v0.4.1"
 
 
 class ImageViewer:
